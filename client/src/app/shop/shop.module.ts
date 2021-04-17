d2a6724ae1554540a46c9a90f70a5dbc6f11a935
Daniel Rae <maildanielrae@gmail.com>
2021-04-17 10:15:23 +0100

    End of section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,6 +4,8 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+//import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
@@ -11,8 +13,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ShopRoutingModule
   ],
-  exports: [ShopComponent]
+  //exports: [ShopComponent]
 })
 export class ShopModule { }
